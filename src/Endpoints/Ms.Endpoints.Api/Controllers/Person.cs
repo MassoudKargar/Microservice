e7253f9fc35f1b40869302e2b2b73c67887321ec
MassoudKargar <75643194+MassoudKargar@users.noreply.github.com>
2024-04-16 23:26:20 +0330

    refactor: Update DbContextModelSnapshot and migration for Person and PhoneNumber entities
    The changes include adding property configurations and navigation properties for the Person and PhoneNumber entities in the DbContextModelSnapshot file. Additionally, a new migration file was added to create tables for People and PhoneNumber with proper constraints and indexes.

---
+++ src/Endpoints/Ms.Endpoints.Api/Controllers/Person.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+using Ms.Core.ApplicationServices;
+
+namespace Ms.Endpoints.Api.Controllers;
+
+[Route("api/[controller]")]
+[ApiController]
+public class Person(PersonApplicationService applicationService) : ControllerBase
+{
+    private PersonApplicationService ApplicationService { get; } = applicationService;
+
+    [HttpPost]
+    public IActionResult AddPerson(CreatePersonInputDto createPersonInputDto)
+    {
+        ApplicationService.AddPerson(createPersonInputDto);
+        return Ok();
+    }
+    [HttpPut]
+    public IActionResult AddNumberToPerson(AddNumberToPersonInputDto inputDto)
+    {
+        ApplicationService.AddNumberToPerson(inputDto);
+        return Ok();
+    }
+}
--- src/Infrastructure/Data/Ms.Infrastructure.Data.SqlServer/ApplicationDbContext.cs
+++ src/Infrastructure/Data/Ms.Infrastructure.Data.SqlServer/ApplicationDbContext.cs
@@ -8,6 +8,6 @@ public class ApplicationDbContext : DbContext
     public DbSet<Person> People { get; set; }
     protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
     {
-        optionsBuilder.UseSqlServer("Server=.; Initial Catalog=Microservice; User Id=sa; Password=M@$$0ud1001;");
+        optionsBuilder.UseSqlServer("Server=.; Initial Catalog=Microservice; User Id=masoud; Password=M@$$0ud1001;Encrypt=False;Trust Server Certificate=False;");
     }
 }
