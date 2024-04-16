1544ce1d32672f962e5e99098af48bf8a7882a70
MassoudKargar <75643194+MassoudKargar@users.noreply.github.com>
2024-04-16 22:20:35 +0330

    feat: Add PhoneNumber and Person classes to Ms.Core.Domain namespace
    Added PhoneNumber and Person classes to the Ms.Core.Domain namespace.
    PhoneNumber class includes properties for Id and Number.
    Person class includes properties for Id, FirstName, LastName, and PhoneNumbers.
    Both classes have constructor parameters with null checks.
    Updated Ms.Core.Domain.csproj to target net8.0 with nullable enabled.

---
+++ src/Core/Ms.Core.Domain/Person.cs
@@ -0,0 +1,19 @@
+﻿namespace Ms.Core.Domain;
+
+public class Person
+{
+    private Person()
+    {
+        
+    }
+    public Person(string firsName, string lastName, List<PhoneNumber> phoneNumbers)
+    {
+        FirsName = firsName ?? throw new ArgumentNullException(nameof(firsName));
+        LastName = lastName ?? throw new ArgumentNullException(nameof(lastName));
+        PhoneNumbers = phoneNumbers ?? throw new ArgumentNullException(nameof(phoneNumbers));
+    }
+    public int Id { get; set; }
+    public string FirsName { get; set; }
+    public string LastName { get; set; }
+    public List<PhoneNumber> PhoneNumbers { get; set; } = new();
+}
