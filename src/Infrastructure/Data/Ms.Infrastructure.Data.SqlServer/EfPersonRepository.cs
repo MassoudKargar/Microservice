8031cadbebb1bfcd41562a3a9c650f2dd22b3f83
MassoudKargar <75643194+MassoudKargar@users.noreply.github.com>
2024-04-16 23:07:43 +0330

    feat: Add Entity Framework implementation for PersonRepository
    Added EfPersonRepository class to handle CRUD operations for Person entity.
    Implemented Add, Get, and Update methods to interact with the database.
    Updated the ApplicationDbContext to include DbSet for the Person entity.
    Updated the project file with necessary package and project references.

---
+++ src/Infrastructure/Data/Ms.Infrastructure.Data.SqlServer/EfPersonRepository.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Ms.Core.ApplicationServices;
+using Ms.Core.Domain;
+
+namespace Ms.Infrastructure.Data.SqlServer;
+
+public class EfPersonRepository(ApplicationDbContext dbContext) : IPersonRepository
+{
+    private ApplicationDbContext DbContext { get; } = dbContext;
+
+    public void Add(Person person)
+    {
+        DbContext.People.Add(person);
+        DbContext.SaveChanges();
+    }
+
+    public Person? Get(int id)
+    {
+        var person = DbContext.People.Include(c => c.PhoneNumbers).SingleOrDefault(c => c.Id == id);
+        return person;
+    }
+
+    public void Update(Person person)
+    {
+        DbContext.SaveChanges();
+    }
+}
