c6e457b35c4e7fd24a354a85e7a826183553db4a
MassoudKargar <75643194+MassoudKargar@users.noreply.github.com>
2024-04-25 00:15:55 +0330

    feat: Add FlightBooking validation and specification classes
    Added FlightBooking validation and specification classes with necessary
    properties and methods to ensure proper validation and specification
    criteria are met. Included new classes for OrSpesification,
    AndSpesification, NotSpesification, RescheduleValidationSatisfied.
    Updated project files to target net8.0 framework.

---
+++ 10. EntitiesSample/Ms.EntitiesSample.IdGenerators.Snowflakes/SnowflakeIdGenerator.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.Extensions.Configuration;
+using Ms.EntitiesSample.IdGenerators.Contract;
+using Snowflake.Core;
+
+namespace Ms.EntitiesSample.IdGenerators.Snowflakes;
+public class SnowflakeIdGenerator : IdGenerator
+{
+    private readonly IdWorker _idWorker;
+    public SnowflakeIdGenerator(IConfiguration configuration)
+    {
+        int datacenterId = int.Parse(configuration["datacenterId"]);
+        _idWorker = new IdWorker(datacenterId, 1);
+
+    }
+
+    public long Next()
+    {
+        return _idWorker.NextId();
+    }
+}
