c707d20470f0ae68747f61af325107c56d2649c8
MassoudKargar <75643194+MassoudKargar@users.noreply.github.com>
2024-04-26 23:29:57 +0330

    refactor: Change Version property to be private in AggregateRoot
    This commit updates the Version property in the AggregateRoot class to be
    private, ensuring that it can only be modified internally within the class
    itself. This change improves encapsulation and data integrity within the
    AggregateRoot class.

--- 14. EventSourcingSample/Ms.EventSourcingSample.Domain/Products/Entities/Product.cs
+++ 14. EventSourcingSample/Ms.EventSourcingSample.Domain/Products/Entities/Product.cs
@@ -5,50 +5,44 @@ namespace Ms.EventSourcingSample.Domain.Products.Entities;
 
 public class Product : AggregateRoot
 {
-    public Product(long id, string name, string description, int price)
-    {
-        Id = id;
-        Name = name;
-        Description = description;
-        Price = price;
-        AddEvent(new ProductCreated(id, name, description, price));
-    }
-
-    public Product(IReadOnlyList<IDomainEvent> domainEvents)
-    {
-        foreach (var @event in domainEvents)
-        {
-            switch (@event)
-            {
-                case ProductCreated pc:
-                    Id = pc.Id;
-                    Name = pc.Name;
-                    Description = pc.Description;
-                    Price = pc.Price;
-                    break;
-                case PriceChanged priceChanged:
-                    Price = priceChanged.Price;
-                    break;
-                case NameChanged nameChanged:
-                    Name = nameChanged.Name;
-                    break;
-            }
-            Version++;
-        }
-    }
     public long Id { get; set; }
     public string Name { get; set; }
     public string Description { get; set; }
     public int Price { get; set; }
 
+    public Product(long id, string name, string description, int price)
+    {
+        Apply(new ProductCreated(id, name, description, price));
+    }
+
+    public Product(IReadOnlyList<IDomainEvent> domainEvents) : base(domainEvents)
+    {
+        
+    }
     public void ChangePrice(int newValue)
     {
-        Price = newValue;
-        AddEvent(new PriceChanged(Id,newValue));
+        Apply(new PriceChanged(Id,newValue));
     }
     public void ChangeName(string newName)
     {
-        Name = newName;
-        AddEvent(new NameChanged(Id, newName));
+        Apply(new NameChanged(Id, newName));
+    }
+
+    public void On(ProductCreated productCreated)
+    {
+        Id = productCreated.Id;
+        Name = productCreated.Name;
+        Description = productCreated.Description;
+        Price = productCreated.Price;
+    }
+
+    public void On(PriceChanged priceChanged)
+    {
+        Price = priceChanged.Price;
+    }
+
+    public void On(NameChanged nameChanged)
+    {
+        Name = nameChanged.Name;
     }
 }
--- 14. EventSourcingSample/Ms.EventSourcingSample.Framework/AggregateRoot.cs
+++ 14. EventSourcingSample/Ms.EventSourcingSample.Framework/AggregateRoot.cs
@@ -3,12 +3,38 @@
 public class AggregateRoot : Entity
 {
     private readonly List<IDomainEvent> _events = new List<IDomainEvent>();
-    public int Version { get; protected set; }
+    public int Version { get; private set; }
     public IReadOnlyList<IDomainEvent> Events => _events;
     protected void AddEvent(IDomainEvent @event)
     {
         _events.Add(@event);
     }
+
+    protected void Apply(IDomainEvent @event)
+    {
+        Mutate(@event);
+        AddEvent(@event);
+    }
+
+    public AggregateRoot()
+    {
+        
+    }
+    public AggregateRoot(IReadOnlyList<IDomainEvent> domainEvents)
+    {
+        if (domainEvents == null || domainEvents.Count < 1)
+        {
+            return;
+        }
+
+        foreach (var domainEvent in domainEvents)
+        {
+            Mutate(domainEvent);
+            Version++;
+        }
+
+    }
+    private void Mutate(IDomainEvent @event) => ((dynamic)this).On((dynamic)@event);
 
     public void ClearEvent()
     {
