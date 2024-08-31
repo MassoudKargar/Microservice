f44407e4e21ee78e3e80be4bdb8d6a6ca78bb197
MassoudKargar <75643194+MassoudKargar@users.noreply.github.com>
2024-09-01 00:29:07 +0330

    refactor: Simplify Purchase method in Customer class and add tests for purchase success and failure

---
+++ 22. WhatIsUnitTestingSamples/Ms.WhatIsUnitTestingSamples.Domain.Test/CustomerTestLondon.cs
@@ -0,0 +1,31 @@
+﻿namespace Ms.WhatIsUnitTestingSamples.Domain.Test;
+
+public class CustomerTestLondon
+{
+
+    [Fact]
+    public void Purchase_Done_Correct_Whene_Store_Has_Enough_Inventory()
+    {
+        var customer = new Customer();
+        var product = new Product()
+        {
+            Name = "Livan"
+        };
+        var store = new StoreForValidPurchase();
+         var result = customer.Purchase(store, product, 10);
+        Assert.True(result);
+    }
+    [Fact]
+    public void Purchase_Failed_Where_Store_Has_Not_Enough_Inventory()
+    {
+        var customer = new Customer();
+        var product = new Product()
+        {
+            Name = "Livan"
+        };
+        var store = new StoreForFailedPurchase();
+        var result = customer.Purchase(store, product, 10);
+        Assert.False(result);
+
+    }
+}
