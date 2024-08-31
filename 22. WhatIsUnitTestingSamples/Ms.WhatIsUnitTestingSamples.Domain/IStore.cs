bc2389c08c6a5fd8e7dcbb64284257b70254cef0
MassoudKargar <75643194+MassoudKargar@users.noreply.github.com>
2024-08-31 23:07:48 +0330

    edit 8-31-24

---
+++ 22. WhatIsUnitTestingSamples/Ms.WhatIsUnitTestingSamples.Domain/IStore.cs
@@ -0,0 +1,9 @@
+﻿namespace Ms.WhatIsUnitTestingSamples.Domain;
+
+public interface IStore
+{
+    void AddProduct(Product product, int count);
+    bool HasEnoughtInventory(Product product, int count);
+    int Inventory(Product product);
+    void RemoveProduct(Product product, int count);
+}
